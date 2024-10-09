8cd7a503cc9fccef9844b3bcc192d35b947085cb
Kiarash <kkia2@myseneca.ca>
2024-10-09 18:25:51 -0400

    Adding Number Guessing Simple Game in Window Prompt

---
+++ NumGuessingGame/index.js
@@ -0,0 +1,30 @@
+const minNum = 1;
+const maxNum = 100;
+const answer = 5 
+
+console.log(answer);
+
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running) {
+    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+    guess = Number(guess);
+
+    if(isNaN(guess)) {
+        window.alert("Please enter a valid number");
+    }
+    else if(guess < minNum || guess > maxNum) {
+        window.alert("Please enter a valid number between the range");
+    }
+    else {
+        attempts++;
+        if(guess == answer) {
+            let attemptWord = attempts > 1 ? "attempts" : "attempt";
+            window.alert(`Congrats! It took you ${attempts} ${attemptWord} to guess the right one!`); 
+        }
+    }
+
+}
